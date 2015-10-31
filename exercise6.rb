a2fcd94c04ba88816acd53a941084c355daaad55
carlosprovisorio <carlosprovisorio@icloud.com>
2015-10-31 15:56:31 -0400

    improved version, exercise6

--- exercise6.rb
+++ exercise6.rb
@@ -1,45 +1,46 @@
 
-# Grocery list
+# 1- Grocery list
 
-$grocery_list = ["tomato souce", "pasta", "cheese", "basil", "cream", "salmon"]
+grocery_list = ["tomato souce", "pasta", "cheese", "basil", "cream", "salmon", "bananas"]
 
 # Print Alphabetically
 
-def print
-	$grocery_list.sort.each do |item|
-	puts "* #{item}"
+def print(list) 
+	list.sort.each do |item|
+		puts "* #{item}"
 	end 
 	puts " "
 end
 
-print
+print(grocery_list)
 
-# I forgot rice
-$grocery_list.push("rice")
+# 2- I forgot rice
+grocery_list.push("rice")
  
-print
+print(grocery_list)
 
-#How many itens there are my list
+# 3-How many itens there are my list
 
-puts "My list contains #{$grocery_list.count} itens."
+puts "My list contains #{grocery_list.count} itens."
 puts " "
 
 
-# Do I have bananas?
+# 4 - Do I have bananas?
 
-	if  $grocery_list.include? "bananas"
+	if  grocery_list.include? "bananas"
 		puts "You need pick up bananas."
 	else 
 		puts "You don't need to pick up bananas today."
 	end
+puts " "	
+
+# 5- Print the second item in my list
+
+puts "second item is #{grocery_list[1]}"
 puts " "
 
-#Print the second item in my list
+# 7- delete salmon
+grocery_list.delete("salmon")
 
-puts "second item is #{$grocery_list[1]}"
-puts " "
-
-$grocery_list.delete("salmon")
-
-print
+print(grocery_list)
 
