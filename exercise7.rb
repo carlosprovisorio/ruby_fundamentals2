dd04d000b92c62c07102dfe0aa3522aa57a45df3
carlosprovisorio <carlosprovisorio@icloud.com>
2015-10-31 17:37:50 -0400

    New version, exercise7

--- exercise7.rb
+++ exercise7.rb
@@ -1,27 +1,60 @@
 #Part I
-$students = {
+
+students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
 
-def print 
-	$students.each do |key, value|
-	puts "#{key}: #{value} students"
-
+def print_classroom(students)
+	students.each do |key, value|
+		puts "#{key}: #{value} students"
 	end
 	puts " "
 end
-print
+print_classroom(students)
 
 #Part II
 
-$students[:cohort4] = 43
-print
+students[:cohort4] = 43
+print_classroom(students)
 
 #Part III
 
-puts "#{$students.key}"
+puts students.keys
+
+#part IV
+
+def add_5percent(students_increase)
+	students_increase.each do |key, value|
+		students_increase[key] = 1.05 * value
+	end
+end
+
+add_5percent(students)
+print_classroom(students)
+
+#Part 5
+
+students.delete(:cohort2)
+print_classroom(students)
+
+
+#part6 - BONUS
+
+total = 0.0
+students.each do |key, value|
+ total = total + value
+end
+
+puts "The total amount is #{total}"
+
+
+
+
+
+
+
 
 
 
