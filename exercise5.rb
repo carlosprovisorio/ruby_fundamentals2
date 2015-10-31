16b56fd054eb539a9a95833628b73505682e0a03
carlosprovisorio <carlosprovisorio@icloud.com>
2015-10-31 15:19:25 -0400

    new version.exercise5

--- exercise5.rb
+++ exercise5.rb
@@ -1,9 +1,11 @@
 puts "Enter temperature in Fahrenheit"
 temp = gets.chomp.to_i
 
-def Fahrenheit(temp)
-	(temp - 32) * 5/9
+
+def fahrenheit(f)
+	(f - 32) * 5/9
 end
- puts "The reletad temperature in Celsius is #{Fahrenheit(temp)}"
+
+puts "The reletad temperature in Celsius is #{fahrenheit(temp)}"
 
 
